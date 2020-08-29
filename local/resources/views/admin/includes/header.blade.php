9932b59f72fb88fbccd4552c1d54cb88971c0356
Virendra Kumar <development.vk@hotmail.com>
2020-08-29 23:25:23 +0530

    Optimized Code

--- local/resources/views/admin/includes/header.blade.php
+++ local/resources/views/admin/includes/header.blade.php
@@ -5,7 +5,7 @@
         <!-- mini logo for sidebar mini 50x50 pixels -->
         <span class="logo-mini"><img height="40" src="{{ cdnLink('logo/logo.png', true) }}" /></span>
         <!-- logo for regular state and mobile devices -->
-        <span class="logo-lg"><img height="40" src="{{ cdnLink('logo/logo.png', true) }}" /></span>
+        <span class="logo-lg"><img height="40" src="{{ cdnLink('logo/logo.png', true) }}" /> <b>{{ cpTrans('app_name') }}</b></span>
     </a>
     <!-- Header Navbar: style can be found in header.less -->
     <nav class="navbar navbar-static-top">
